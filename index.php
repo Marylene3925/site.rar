88df736483d97a6aa76b06a9aea8b2a3a7121ac8
Marylene3925 <58249807+Marylene3925@users.noreply.github.com>
2020-05-07 23:35:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -439,8 +439,7 @@
                                     echo "</div>";  
                                      
                                     
-                                ?>
-                           
+                                ?>                           
 
                             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
                             <script>
